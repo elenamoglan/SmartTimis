c009d195088ad33af75b2e6bff6e498b16c6184b
Helen <moglan.elena@gmail.com>
2026-01-14 15:20:31 +0200

    update

--- backend/src/utils/initDb.js
+++ backend/src/utils/initDb.js
@@ -38,11 +38,23 @@ const createTables = async () => {
     );
   `;
 
+  const likesTable = `
+  CREATE TABLE IF NOT EXISTS issue_likes (
+      user_id INTEGER NOT NULL REFERENCES users(id),
+      issue_id INTEGER NOT NULL REFERENCES issue_reports(id),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (user_id, issue_id)
+);
+
+  ALTER TABLE issue_reports ADD COLUMN IF NOT EXISTS likes_count INTEGER DEFAULT 0;
+  `
+
 
   try {
     await db.query(userTable);
     await db.query(issueTable);
     await db.query(notificationTable);
+    await db.query(likesTable);
     console.log('Tables initialized successfully');
   } catch (err) {
     console.error('Error initializing tables', err);
